1636c5842d8ed4008e42f6655ac04fc2f247c0ab
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-29 17:28:09 -0400

    created model/user.js schema

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "You must enter a username!",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "You must enter an email!",
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+//VIRTUALS GO HERE
+//NOTE: modified from model/Pizza.js in pizza-hunt. ask for verification on accuracy
+// UserSchema.virtual("friendCount").get(function () {
+//   return this.friends.reduce(
+//     (total, friend) => total + this.friends.length + 1,
+//     0
+//   );
+// });
+
+const User = model("User", UserSchema);
+
+module.exports = User;
