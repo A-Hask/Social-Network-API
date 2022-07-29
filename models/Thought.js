f8edfafdd5f2a5712f791692fe46d866b999f7a9
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-29 17:46:48 -0400

    created reaction schema in model/thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max: 280,
+    },
+    //should this be writtenBy like in pizza-hunt model/comment.js?
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -18,7 +47,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: "Please enter a username!",
     },
-    reactions: [reactionSchema],
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
